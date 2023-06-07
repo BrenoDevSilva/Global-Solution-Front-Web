c56e9f6a4f01d46e605799e3950fb13f049f040d
Breno Silva <brenosilvats18@gmail.com>
2023-06-07 17:14:53 -0300

    Global Solution Web Site

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ const relogio = setInterval(function time() {
     segundos.textContent = s;
 
 })
+
+function Formulario(){
+    var nome = document.getElementById('nome_form').value;
+    var Email = document.getElementById('email_form').value;
+    var Telefone = document.getElementById('fone_form').value;
+    var opiniao = document.getElementById('opinião').value;
+    if (nome == "" || Email == "" || Telefone == "" || opiniao == ""){
+        alert("Preencha todos os campos, por favor!");
+    } else {
+        alert("Formulário preenchido com sucesso!");
+    }
+}
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+setInterval(nextSlider, 5000)
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
